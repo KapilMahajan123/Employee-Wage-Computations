945f83ceee3eecf87ac7ad0a02ab1233aa61eede
kapilmahajan123 <montyempire123gmail.com>
2022-12-02 20:12:06 +0530

    Added: Checking to the Daily Employee Wage

--- src/com/empWage/java/EmployeeWage.java
+++ src/com/empWage/java/EmployeeWage.java
@@ -10,13 +10,18 @@ import java.util.Random;
 public class EmployeeWage {
 	
 	/*
-	 * check Employee is present or Absent using random
+	 * Calculate Daily Employee Wage assuming wage per hour is 20 and full day hour is 8
 	 */
 	 public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		Random random = new Random();   //random object
+		//input
+		int wagePerHour = 20;
+		int fullDayHours = 8;
+		int salary = 0;
+		
 		/*
 		 * will give 0 or 1 random number
 		 */
@@ -24,11 +29,15 @@ public class EmployeeWage {
 		
 		/*
 		 * condition for present or absent
+		 * Calculate and getting this salary 
 		 */
-		if (randomNum == 1)
+		if (randomNum == 1) {
 			System.out.println("Employee  is Present");
+			salary = wagePerHour * fullDayHours;
+		}
 		else
 			System.out.println("Employee  is Absent");
+			System.out.println("salary is: "+salary);
 
 	}
 
